45af378ad46b86592189c9489fce2ee857699e75
DELL <DELL@WELCOME>
2021-01-13 11:32:23 +0530

    Push the code

---
+++ OrangeHRM2007/src/main/java/OrangeHRM2007/OrangeHRM2007/App.java
@@ -0,0 +1,13 @@
+package OrangeHRM2007.OrangeHRM2007;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ OrangeHRM2007/src/test/java/Vamsi2007/NewTest_11.java
@@ -0,0 +1,9 @@
+package Vamsi2007;
+
+import org.testng.annotations.Test;
+
+public class NewTest_11 {
+  @Test
+  public void f() {
+  }
+}
